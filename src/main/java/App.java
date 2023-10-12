81bf53193d96b7621812ff8eef8e861417977888
shevelev <shevelve@gmail.com>
2023-10-12 11:44:00 +0600

    add main class in App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,21 @@
 import controllers.RootController;
 import io.ebean.DB;
 import io.ebean.Database;
+import io.ebean.annotation.Platform;
+import io.ebean.dbmigration.DbMigration;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import java.io.IOException;
+
 
 public class App {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+//        DbMigration dbMigration = DbMigration.create();
+//        dbMigration.addPlatform(Platform.POSTGRES, "psql");
+//        dbMigration.generateMigration();
 
         Database database = DB.getDefault();
         Javalin app = Javalin.create(
